bd0c9de1729bad9de09d1b75c3b1991af533c899
KsiuTretyakova <hrytsenkoks@gmail.com>
2023-06-19 19:37:39 +0300

    add def talk

--- JARVIS.py
+++ JARVIS.py
@@ -1,1 +1,21 @@
-import speach_recognition as sr
+import speech_recognition as sr
+import os
+import sys
+
+# Якщо немає звуку
+# pip install pyttsx3
+import pyttsx3
+engine = pyttsx3.init()
+# engine.say("Say")
+# engine.runAndWait()
+
+
+def talk(words):
+    print(words)
+    engine.say(words)
+    engine.runAndWait()
+    # os.system("say " + words)
+
+
+talk("Hi, can I help you?")
+
