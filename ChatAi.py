d9d37636a8b2a5ae8bc6e984c6c35ba9e3a65ee9
arseniy876 <arseniy.chp@gmail.com>
2023-06-26 18:42:07 +0200

    add task

---
+++ ChatAi.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv as ld
+import os
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+
+openai.api.key = os.getenv("api_key")
