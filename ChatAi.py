039312c0a8c72b8305a7d782731d16385b83bc3d
arseniy876 <arseniy.chp@gmail.com>
2023-06-26 18:42:57 +0200

    add task

--- ChatAi.py
+++ ChatAi.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv as ld
 import os
 
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+dotenv_path = os.path.join(os.path.dirname(__file__), "./.env")
 if os.path.exists(dotenv_path):
     ld(dotenv_path)
 
