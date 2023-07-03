1fde7a0f0939cf27d04141eecec3b6dd50b7b032
arseniy876 <arseniy.chp@gmail.com>
2023-07-03 18:30:48 +0200

    voice updates

--- JARVIS.py
+++ JARVIS.py
@@ -5,6 +5,19 @@ import sys
 
 # Якщо немає звуку
 # pip install pyttsx3
+import openai
+
+from dotenv import load_dotenv as ld
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    ld(dotenv_path)
+
+openai.api_key = os.getenv("api_key")
+def handle_input(user_input):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}])
+    return completion
+
 import pyttsx3
 engine = pyttsx3.init()
 # engine.say("Say")
@@ -29,22 +42,33 @@ def command():
         audio = r.listen(source)
 
     try:
-        task = r.recognize_google(audio, language='ua-UA').lower()
+        task = r.recognize_google(audio, language='eng-ENG').lower()
         print("Ви проговорили: " + task)
 
     except sr.UnknownValueError:
-        talk("Я вас не зрозумів")
+        #talk("Я вас не зрозумів")
+        talk("Ya vas ne zrozumiv")
         task = command()
 
     return task
 
 
 def make_something(task):
-    if "відкрий сайт" in task:
+    if "open" and "site" in task:
+    #if "відкрий сайт" in task:
         talk ("Відкуриваю")
         url = 'https://it-univer.thecabinet.io/'
         webbrowser.open(url)
-    pass
+    elif "name" and "your" in task:
+        talk("My name is JATVIS")
+
+    elif "stop" in task:
+        talk("Goodbye")
+        sys.exit()
+
+    else:
+        ai_response = handle_input(task).choices[0].message.content
+        talk(ai_response)
 
 
 while True:
